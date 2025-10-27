e71f3c88d16b8347f7c6188663f928bdfacd1dca
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-10-27 15:25:58 +0100

    Update vacature-plugin.php

--- vacature-plugin.php
+++ vacature-plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Vacature Plugin
  * Description: A plugin to manage job applications 
- * Version: 1.2.0
+ * Version: 1.3.0
  * 
  * Author: Abderrahman bakkouy
  * Text domain: vacature-plugin
